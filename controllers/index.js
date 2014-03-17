6ddb7745eb808da98a627e166eb0e02f225e4163
Victor <victordahlin@gmail.com>
2014-03-17 20:35:37 +0100

    admin page
    add new questions

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/quizDB');
+var db = monk('localhost:27017/QuizDB');
 
 require('../models/createDB');
 
@@ -15,18 +15,74 @@ module.exports.controller = function(app) {
   });
 
   app.post('/login', function(req, res){
+
       var user = req.body.username;
       var pass = req.body.password;
+      var admin = db.get('Administrators');
+      //admin.insert({name : user, password : pass });
+      admin.find({name : user, password : pass}, {}, function(e, users){
+        console.log(users.lenght);
+        if(typeof users.lenght == 'undefined'){
+            res.render('index', { title: 'Error, wrong user' });
+        }
+        else{
+           console.log(users.lenght);
+           res.redirect('../highscore');
+        }
+
+      });
+
+   
   });
 
 
   app.get('/highscore', function(req,res) {
-      res.render('highscore', { title: 'Highscore' });
+      var highscore = db.get('Highscore');
+
+      highscore.find({},{},function(e,scores){
+        res.render('highscore', {
+          'highscore' : scores
+        });
+      });
   });
 
 
-  app.get('/admin', function(req,res) {
-      res.render('admin', { title: 'Administration' });
+  app.get('/addQuestion', function(req,res) {
+      var game = db.get('GameQuestions');
+      
+      game.find({},{},function(e,question){
+        res.render('admin', { 
+          title           : 'Add question',
+          'questions'     : question
+        });
+      });
+  });
+
+  app.post('/addQuestion', function(req,res) {
+    var game = db.get('GameQuestions');
+
+    var q = req.body.question;
+    var fst = req.body.one;
+    var snd = req.body.cross;
+    var thrd = req.body.two;
+    var ans = req.body.answere;
+
+    //console.log(q + " " + fst + " " + snd + " " + thrd + " " + ans);
+    /*  admin.jade
+        h3= chooseQuestions  
+        select
+          each q, i in questions
+            option(value=q.id)=q.id
+    */
+
+    game.insert({
+      question : q, 
+      one : fst,
+      cross : snd, 
+      two : thrd,
+      answere : ans
+    });
+    res.redirect('/addQuestion');
   });
 
 }
